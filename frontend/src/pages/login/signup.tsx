4f46b6dd3157bffba5b577c3a01f3c38dc9ae778
NimaShariatz <shariatz@ualberta.ca>
2026-07-13 03:28:32 -0600

    work

--- frontend/src/pages/login/signup.tsx
+++ frontend/src/pages/login/signup.tsx
@@ -40,41 +40,62 @@ function Signup() {
 
     <div className={styles.content}>
     <div className={styles.contentContainer}>
+      <div className={styles.yellowSquare}></div>
 
       <div className={styles.formContainer}>
-        <div className={styles.titleContainer}>
-          <h3>Kunkleman<span>Chevrolet</span></h3>
 
+        <div className={styles.titleContainer}>
+          <h2>Kunkleman<span>Chevrolet</span></h2>
         </div>
 
+        <h5>Sign Up</h5>
 
         <form action={handleSubmit}>
-          <label>First Name</label>
-          <input required type="text" name="first_name" placeholder="first name" autoComplete="First Name" />
+        <div className={styles.rowColumn}>
+          <div className={styles.formRow}>
+            <div>
+              <label>First Name</label>
+              <input required type="text" name="first_name" placeholder="First Name" autoComplete="First Name" />
+            </div>
 
-          <label>Last Name</label>
-          <input required type="text" name="last_name" placeholder="last name" autoComplete="Last Name" />
+            <div>
+              <label>Last Name</label>
+              <input required type="text" name="last_name" placeholder="Last Name" autoComplete="Last Name" />
+            </div>
+          </div>
 
-          <label>Email</label>
-          <input required type="email" name="email" placeholder="Email" autoComplete="email" />
+          <div className={styles.formItem}>
+            <label>Email</label>
+            <input required type="email" name="email" placeholder="Email" autoComplete="email" />
+          </div>
 
-          <label>Password</label>
-          <input required type="password" name="password" minLength={9} placeholder="Password" autoComplete="new-password" />
+          <div className={styles.formItem}>
+            <label>Password</label>
+            <input required type="password" name="password" minLength={9} placeholder="Password" autoComplete="new-password" />
+          </div>
 
-          <label>Street Name</label>
-          <input required type="text" name="street_name" placeholder="Street Name" />
+          <div className={styles.formItem}>
+            <label>Street Name</label>
+            <input required type="text" name="street_name" placeholder="Street Name" />
+          </div>
 
-          <label>City</label>
-          <input required type="text" name="city" placeholder="City" />
+          <div className={styles.formItem}>
+            <label>City</label>
+            <input required type="text" name="city" placeholder="City" />
+          </div>
 
-          <label>Province</label>
-          <input required type="text" name="province" placeholder="Province" />
+          <div className={styles.formItem}>
+            <label>Province</label>
+            <input required type="text" name="province" placeholder="Province" />
+          </div>
 
-          <label>Postal Code</label>
-          <input required type="text" name="postal_code" placeholder="A1A 1A1" maxLength={7} />
-
+          <div className={styles.formItem}>
+            <label>Postal Code</label>
+            <input required type="text" name="postal_code" placeholder="A1A 1A1" maxLength={7} />
+          </div>
           {error && <p>{error}</p>}
-          <button type="submit">Sign Up</button>
+          <button className={styles.signupButton} type="submit">Sign Up</button>
+         </div>
         </form>
       </div>
 
